744e29c1eebaf2330487ed1913e7cf179052e434
Daniel <danielpachecoamorim@gmail.com>
2025-10-23 19:42:35 -0300

    Finalizando aula09

--- aulas/aula09/tests/tarefaRouter.test.js
+++ aulas/aula09/tests/tarefaRouter.test.js
@@ -69,6 +69,12 @@ describe('Testes do recurso /tarefas',() => {
         expect(response.body.msg).toBe("Tarefa não encontrada");
     });
 
+    test('PUT / id deve retornar 422', async () => {
+        const response = await request.put(`${url}/${id}`).send({nome: "", concluida: true});
+        expect(response.status).toBe(422);
+        expect(response.body.msg).toBe("Nome da tarefa é obrigatorio")
+    });
+
     test('DELETE / id deve retornar 204', async () => {
         const response = await request.delete(`${url}/${id}`);
         expect(response.status).toBe(204);
